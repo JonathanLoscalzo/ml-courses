dbd7a08746d361b69d3c7e8d30c3b7503ed7ec12
Jonathan Loscalzo <jloscalzo@hexacta.com>
2020-02-04 08:59:37 -0300

    unsupervised chp2

--- DataCamp/unsupervised learning/01.py
+++ DataCamp/unsupervised learning/01.py
@@ -85,5 +85,81 @@ print(ct)
 # on the type of data you are working with, the clustering may not always be this good. 
 
 
+#########################
 
- 
+## Transforming features for better clustering: 
+variance of a feature measure spread of its values
+
+# Perform the necessary imports
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+# Create scaler: scaler
+scaler = StandardScaler()
+
+# Create KMeans instance: kmeans
+kmeans = KMeans(n_clusters=4)
+
+# Create pipeline: pipeline
+pipeline = make_pipeline(scaler, kmeans)
+
+# You'll now use your standardization and clustering pipeline from the previous exercise 
+# to cluster the fish by their measurements, and then create a cross-tabulation to compare the cluster labels with the fish species.
+
+# Import pandas
+import pandas as pd
+
+# Fit the pipeline to samples
+pipeline.fit(samples)
+
+# Calculate the cluster labels: labels
+labels = pipeline.predict(samples)
+
+# Create a DataFrame with labels and species as columns: df
+df = pd.DataFrame({'labels': labels, 'species':species})
+
+# Create crosstab: ct
+ct = pd.crosstab(df.labels, df.species)
+
+# Display ct
+print(ct)
+
+######################
+
+# Note that Normalizer() is different to StandardScaler(), 
+# which you used in the previous exercise. 
+# While StandardScaler() standardizes features 
+# (such as the features of the fish data from the previous 
+# exercise) by removing the mean and scaling to unit variance, 
+# Normalizer() rescales each sample - here, 
+# each company's stock price - independently of the other.
+
+
+# Import Normalizer
+from sklearn.preprocessing import Normalizer
+
+# Create a normalizer: normalizer
+normalizer = Normalizer()
+
+# Create a KMeans model with 10 clusters: kmeans
+kmeans = KMeans(n_clusters=10)
+
+# Make a pipeline chaining normalizer and kmeans: pipeline
+pipeline = make_pipeline(normalizer, kmeans)
+
+# Fit pipeline to the daily price movements
+pipeline.fit(movements)
+
+# Import pandas
+import pandas as pd
+
+# Predict the cluster labels: labels
+labels = pipeline.predict(movements)
+
+# Create a DataFrame aligning labels and companies: df
+df = pd.DataFrame({'labels': labels, 'companies': companies})
+
+# Display df sorted by cluster label
+print(df.sort_values(by='labels'))
+    
