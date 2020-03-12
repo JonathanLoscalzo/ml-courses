3e21ce986ef6e39a12060341f670a2b83b497718
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2020-03-12 00:08:16 -0300

    intro to deep learning keras

---
+++ DataCamp/introduce deep learning/chapter3.py
@@ -0,0 +1,67 @@
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+# Save the number of columns in predictors: n_cols
+n_cols = predictors.shape[1]
+
+# Set up the model: model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(50, activation='relu', input_shape=(n_cols,)))
+
+# Add the second layer
+model.add(Dense(32, activation='relu'))
+
+# Add the output layer
+model.add(Dense(1))
+# ___________________
+
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+# Specify the model
+n_cols = predictors.shape[1]
+model = Sequential()
+model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Verify that model contains information from compiling
+print("Loss function: " + model.loss)
+
+# Fit the model
+model.fit(predictors, target)
+
+#_______________________
+
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import to_categorical
+
+# Convert the target to categorical: target
+target = to_categorical(df.survived)
+
+# Set up the model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(32, activation='relu', input_shape=(n_cols,)))
+
+# Add the output layer
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+model.fit(predictors, target)
